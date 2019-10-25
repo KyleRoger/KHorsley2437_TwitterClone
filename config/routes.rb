4591dd2a8c65ba62e33044b0d61db615710d8586
KyleRoger <kyleroger@hotmail.com>
2019-10-25 20:39:59 +0000

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   
+  root 'twitter_pages#home'
+  
   get 'twitter_pages/home'
 
   get 'twitter_pages/help'
-
-    root 'application#hello'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  get 'twitter_pages/about'
 end
--- test/controllers/twitter_pages_controller_test.rb
+++ test/controllers/twitter_pages_controller_test.rb
@@ -1,14 +1,29 @@
 require 'test_helper'
 
 class TwitterPagesControllerTest < ActionDispatch::IntegrationTest
+    
+  def setup
+    @base_title = "Ruby on Rails Twitter Clone Application"
+  end
+
+  
   test "should get home" do
     get twitter_pages_home_url
     assert_response :success
+    assert_select "title", "Home | #{@base_title}"
+
   end
 
   test "should get help" do
     get twitter_pages_help_url
     assert_response :success
+    assert_select "title", "Help | #{@base_title}"
+  end
+  
+  test "should get about" do
+    get twitter_pages_about_url
+    assert_response :success
+    assert_select "title", "About | #{@base_title}"
   end
 
 end
