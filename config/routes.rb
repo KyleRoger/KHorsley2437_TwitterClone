72182dac4340b54e9795e69cf2abd7fc6c158852
KyleRoger <kyleroger@hotmail.com>
2019-10-26 21:16:38 +0000

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
   root 'twitter_pages#home'
   get  '/help',    to: 'twitter_pages#help'
   get  '/about',   to: 'twitter_pages#about'
   get  '/contact', to: 'twitter_pages#contact'
   get  '/signup',  to: 'users#new'
+  post '/signup',  to: 'users#create'
+  resources :users
 end
