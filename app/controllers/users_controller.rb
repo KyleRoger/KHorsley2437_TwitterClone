5f4f0515a7e035c7094600554d50621c62cd5723
KyleRoger <kyleroger@hotmail.com>
2019-10-27 18:11:06 +0000

    adding post

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
   
   def new
@@ -55,14 +56,6 @@ end
                                    :password_confirmation)
     end
     
-    # Confirms a logged-in user.
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-    end
-
     # Confirms the correct user.
     def correct_user
       @user = User.find(params[:id])
@@ -72,5 +65,13 @@ end
     # Confirms an admin user.
     def admin_user
       redirect_to(root_url) unless current_user.admin?
+    end
+    
+    # Confirms a logged-in user.
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url
     end
 end
